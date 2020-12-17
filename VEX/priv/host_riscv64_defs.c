61b58ffe3147f6ade23d47bdb8c73c71788f78d9
Petr Pavlu <setup@dagobah.cz>
2020-12-17 07:43:12 +0100

    riscv64: Use ABI register names in ppHRegRISCV64()

--- VEX/priv/host_riscv64_defs.c
+++ VEX/priv/host_riscv64_defs.c
@@ -40,7 +40,10 @@
 
 UInt ppHRegRISCV64(HReg reg)
 {
-   Int r;
+   static const HChar* names[32] = {
+      "zero", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
+      "a1",   "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
+      "s6",   "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
    /* Be generic for all virtual regs. */
    if (hregIsVirtual(reg))
@@ -48,10 +51,11 @@ UInt ppHRegRISCV64(HReg reg)
 
    /* Be specific for real regs. */
    switch (hregClass(reg)) {
-   case HRcInt64:
-      r = hregEncoding(reg);
-      vassert(r >= 0 && r < 31);
-      return vex_printf("x%d", r);
+   case HRcInt64: {
+      UInt r = hregEncoding(reg);
+      vassert(r >= 0 && r <= 31);
+      return vex_printf("%s", names[r]);
+   }
    default:
       vpanic("ppHRegRISCV64");
    }
