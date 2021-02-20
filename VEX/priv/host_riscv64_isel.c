7697b86b73fd25c7cf46147b2660029729ae1c60
Petr Pavlu <setup@dagobah.cz>
2021-02-20 19:47:45 +0100

    riscv64: Add isel for Iop_Sub64

--- VEX/priv/host_riscv64_isel.c
+++ VEX/priv/host_riscv64_isel.c
@@ -196,7 +196,6 @@ static HReg iselIntExpr_R_wrk(ISelEnv* env, IRExpr* e)
 
    /* ---------------------- BINARY OP ---------------------- */
    case Iex_Binop: {
-      /* ADD */
       switch (e->Iex.Binop.op) {
       case Iop_Add64: {
          HReg dst  = newVRegI(env);
@@ -204,6 +203,14 @@ static HReg iselIntExpr_R_wrk(ISelEnv* env, IRExpr* e)
          /* TODO Optimize for small imms by generating addi. */
          HReg argR = iselIntExpr_R(env, e->Iex.Binop.arg2);
          addInstr(env, RISCV64Instr_ADD(dst, argL, argR));
+         return dst;
+      }
+      case Iop_Sub64: {
+         HReg dst  = newVRegI(env);
+         HReg argL = iselIntExpr_R(env, e->Iex.Binop.arg1);
+         /* TODO Optimize for small imms by generating addi. */
+         HReg argR = iselIntExpr_R(env, e->Iex.Binop.arg2);
+         addInstr(env, RISCV64Instr_SUB(dst, argL, argR));
          return dst;
       }
       default:
